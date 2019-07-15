86bd5ec906114e391beb792f591113541d546be9
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-15 10:54:10 +0530

    k3

--- KGit3/lambda.js
+++ KGit3/lambda.js
@@ -1,4 +1,17 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let AWS = require('aws-sdk');
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = function (event, context, callback) {
+    cognito_idp.listUsers({
+        UserPoolId: "us-east-1_uVXTQInep",
+        Limit: 10
+    }, function (error, data) {
+        if (error) {
+            console.log(error);
+            throw error;
+        }
+        // your logic goes within this block
+    });
+
+    callback(null, { "message": "Successfully executed" });
 }
